0633389afb9770d09f2a2aede32cecf4d49ce356
Conrad Wright <easp.conrad@gmail.com>
2019-09-04 10:47:01 -0600

    WIP sidebar added

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { ListGroup, ListGroupItem } from 'reactstrap'
+
+import './sidebar.css'
+
+export default class SidebarMenu extends React.Component {
+  handleClick(event) {
+    // change route to appropriate component
+    console.log('clicked: ')
+  }
+
+  render() {
+    return (
+      <div className='sidebar'>
+        <h2 className='sidebar-header'> Options </h2>
+        <ListGroup className='sidebar-list'>
+          <ListGroupItem onClick={this.handleClick}> The Shelf </ListGroupItem>
+          <ListGroupItem onClick={this.handleClick}>The Library</ListGroupItem>
+          <ListGroupItem onClick={this.handleClick}>BookNotes</ListGroupItem>
+          <ListGroupItem onClick={this.handleClick}>
+            Add to the Library
+          </ListGroupItem>
+          <ListGroupItem onClick={this.handleClick}>Other things</ListGroupItem>
+        </ListGroup>
+      </div>
+    )
+  }
+}
